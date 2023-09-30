80fe13b2c8283f502ea53b3c0893f93e88208bb7
martai4 <tusiulka2011@gmail.com>
2023-09-30 15:18:59 +0200

    layout prepared

--- educat-front/educat/src/App.js
+++ educat-front/educat/src/App.js
@@ -5,7 +5,11 @@ export class App extends React.Component{
   render(){
     return (
       <div className="App">
-        App I am
+          <div className='Cat'>
+            <img src="/educat_logo_no_background.png" className='caty'></img>
+          </div>
+          <div className='Messages'>Text</div>
+          <div className='NextIcon'>Strzałka</div>
       </div>
     );
   }
