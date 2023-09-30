5bfb443b85e85b47ee80e4c397c9b555d3fb6d93
marratz <marrtaz.19@gmail.com>
2023-09-30 16:04:31 +0200

    speach bubble

--- educat-front/educat/src/App.js
+++ educat-front/educat/src/App.js
@@ -5,7 +5,12 @@ export class App extends React.Component{
   render(){
     return (
       <div className="App">
-        App I am
+        x
+        <div className='Bubble'> 
+        In the menu bar, in the "For you" tab, you can find useful articles and information about available fields of study at Polish universities.
+
+        </div>
+        
       </div>
     );
   }
