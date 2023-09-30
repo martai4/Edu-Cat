c8561f2c96954c6ca9dc307777bab25ee0b42148
Wojtek <>
2023-09-30 10:46:35 +0200

    Init - backend project structure

---
+++ educat/src/main/java/edu/thechoice/educat/EducatApplication.java
@@ -0,0 +1,13 @@
+package edu.thechoice.educat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducatApplication.class, args);
+	}
+
+}
---
+++ educat/src/test/java/edu/thechoice/educat/EducatApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.thechoice.educat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
