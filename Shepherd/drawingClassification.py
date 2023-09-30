a09ec264cadd3640050eaa7ea78fbdee2ce12f87
Mateusz Kaźmierski <matkis28@gmail.com>
2023-09-30 18:30:48 +0200

    python service without logic

---
+++ Shepherd/drawingClassification.py
@@ -0,0 +1,11 @@
+import tensorflow
+from cv2 import Mat
+
+
+class DrawingClassificator:
+    def __init__(self):
+        pass
+
+    def classify(self, img : Mat):
+
+        return [0,1,2,3,4,5,6,7]
