d65a5eca74f4727b37ce94afe1464c9017102381
Wojtek <>
2023-09-30 23:03:01 +0200

    Categories and image from frontend, personality from python

--- educat/src/main/java/edu/thechoice/educat/features/recommender/Calculator.java
+++ educat/src/main/java/edu/thechoice/educat/features/recommender/Calculator.java
@@ -3,6 +3,7 @@ package edu.thechoice.educat.features.recommender;
 import edu.thechoice.educat.features.database.DatabaseService;
 import edu.thechoice.educat.features.database.domain.Category;
 import edu.thechoice.educat.features.database.domain.EducationChoice;
+import edu.thechoice.educat.features.shepherd.ShepherdClient;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ import java.util.Map;
 public class Calculator {
     @Autowired
     private final DatabaseService dbService;
-    public List<EducationChoice> calculatePreferences(final List<EducationChoice> facultyList, final Map<Category, Integer> userPref, final Map<Personality, Integer> personality){
+    public List<EducationChoice> calculatePreferences(final List<EducationChoice> facultyList, final Map<Category, Integer> userPref, ShepherdClient.PersonalityAnalysis personality){
         for (EducationChoice faculty:facultyList){
             int s = userPref.get(faculty.getCategory());
 //            abs because of randomly generated test data, which generates higher place in ranking than instances in category are
