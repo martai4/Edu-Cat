8d06650074f28309c4a0108cc4f782166a8a31a5
martai4 <tusiulka2011@gmail.com>
2023-10-01 00:09:32 +0200

    repaired

--- educat/src/main/java/edu/thechoice/educat/features/recommender/api/RestApi.java
+++ educat/src/main/java/edu/thechoice/educat/features/recommender/api/RestApi.java
@@ -1,5 +1,7 @@
 package edu.thechoice.educat.features.recommender.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.thechoice.educat.features.database.DatabaseService;
 import edu.thechoice.educat.features.database.domain.Category;
 import edu.thechoice.educat.features.database.domain.EducationChoice;
@@ -11,16 +13,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000","http://127.0.0.1:3000"})
+@Slf4j
 public class RestApi {
 
     private final DatabaseService dbService;
@@ -35,7 +38,7 @@ public class RestApi {
     public List<EducationChoice> discoverFieldOfStudies(
             @RequestParam("json") String preferenceString,
             @RequestParam("image") MultipartFile image
-            ) throws JsonProcessingException {
+    ) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Map<Category, Integer>> pref = mapper.readValue(preferenceString, Map.class);
         Map<Category, Integer> preference = pref.get("userPreferences");
@@ -43,11 +46,7 @@ public class RestApi {
         for (Category cat: Category.values()){
             preference.putIfAbsent(cat, 1);
         }
-//        for(String key: per.keySet()){
-//            personality.put(Personality.valueOf(key), per.get(key));
-//        }
-//        per.clear();
-        pref.clear();
+        ShepherdClient.PersonalityAnalysis personality = client.analize(image);
 
 
         List<EducationChoice> faculties = dbService.getAll();
